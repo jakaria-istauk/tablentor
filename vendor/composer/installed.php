a3b2aa209c4515eabdf57bcaedf5e4a7791405e8
Jakaria Istauk <jakariamd35@gmail.com>
2024-01-17 07:52:07 +0600

    Fatal error issue fixed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jakaria/tablentor',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7c1360dce2af3bcdfb2825e9642dacaef6a9c131',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jakaria/tablentor' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7c1360dce2af3bcdfb2825e9642dacaef6a9c131',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
